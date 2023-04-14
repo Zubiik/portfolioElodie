c7cacbabcd2f4dfc69e8026e2f1f607ecc81fa97
Zubiik <elodiefrappat08@gmail.com>
2023-04-14 17:04:52 +0200

    ajout style navigation

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,20 +4,18 @@ import Tools from '../Tools';
 import Skills from '../Skills';
 import Footer from '../Footer';
 import reactLogo from './react-logo.svg';
-import './styles.css';
+import './styles.scss';
 import Portfolio from '../Portfolio';
 
 function App() {
   return (
     <div className="app">
-      <img src={reactLogo} alt="react logo" />
-      <h1>Bienvenue sur mon nouveau site youpi</h1>
-      <Header />
       <Navigation />
-      <Tools />
+      <Header />
+      {/* <Tools />
       <Skills />
       <Portfolio />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,16 +1,19 @@
 import PropTypes from 'prop-types';
 
-import './style.scss';
+import './styles.scss';
 
 const Navigation = () => {
   console.log('coucou');
   return (
-    <nav>
-      <a>Skills</a>
-      <a>Portfolio</a>
-      <a>Tools</a>
-      <a>Tools</a>
-    </nav>
+    <header className="header">
+      <span>Elodie Frappat</span>
+      <nav className="navigation">
+        <a className="navigation-link">Skills</a>
+        <a className="navigation-link">Portfolio</a>
+        <a className="navigation-link">Tools</a>
+        <a className="navigation-link">Tools</a>
+      </nav>
+    </header>
   );
 };
 
