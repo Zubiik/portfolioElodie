aba106e58d0185845702cca27f94bd8242391947
Zubiik <elodiefrappat08@gmail.com>
2023-04-14 18:02:47 +0200

    accueil stylisation

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,26 @@
 import PropTypes from 'prop-types';
+import avatar from '../../img/avatar.png';
 
 import './style.scss';
 
 const Header = () => (
-  <div>
-    Header
-  </div>
+  <main className="home">
+    <div>
+      <p className="home-title">Développeuse web front-end</p>
+      <p className="home-description">
+        Actuellement à la recherche d'un nouvel emploi, je suis intéressée par
+        les postes de :
+      </p>
+      <p className="home-description">Développeuse front-end & full-stack</p>
+    </div>
+    <section>
+      <img
+        className="home-avatarImage"
+        src={avatar}
+        alt="illustration avatar"
+      />
+    </section>
+  </main>
 );
 
 Header.propTypes = {
